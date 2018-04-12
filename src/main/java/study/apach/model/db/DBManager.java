ec69f8ddc9dd2cebb365cd047893d84e8c5f298b
kovalenko_dmytro <kovalenko.dm80@gmail.com>
2018-04-12 11:50:15 +0300

    change DBManager

--- src/main/java/study/apach/model/db/DBManager.java
+++ src/main/java/study/apach/model/db/DBManager.java
@@ -1,24 +1,51 @@
 package study.apach.model.db;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBManager {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/bookstore_db";
-    private static final String LOGIN = "postgres";
-    private static final String PASSWORD = "apach";
+    private static Properties dbProperties;
+
+    static {
+
+        dbProperties = new Properties();
+
+        try {
+            BufferedReader reader = new BufferedReader(
+                                    new FileReader("src\\main\\resources\\local\\db.properties"));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                String[] propertyLine = line.split("=");
+                dbProperties.setProperty(propertyLine[0], propertyLine[1]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     private DBManager(){};
 
     public static Connection createConnection() {
+
         Connection connection = null;
+
         try {
-            connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+            connection = DriverManager.getConnection(dbProperties.getProperty("host"),
+                                                        dbProperties.getProperty("user"),
+                                                        dbProperties.getProperty("password"));
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return connection;
     }
 }
